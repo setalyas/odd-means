36b2b21e60e50a60ef0c26ab85af3c31388925b8
setalyas <37209008+setalyas@users.noreply.github.com>
2020-01-21 00:50:48 +0000

    First commit
    Added ditherer's mean functions

---
+++ ditherers_mean.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 09:06:58 2020
+
+@author: setat
+"""
+
+"""To take the ditherer’s mean of n numbers, we want an iterative process
+that gives us n numbers at each step. So at each step, we replace the
+smallest number from the previous list of numbers with the geometric mean
+of the previous numbers and the largest number with the arithmetic mean of
+the numbers. - Evelyn Lamb"""
+ 
+from scipy.stats import gmean
+import numpy as np
+
+trial = [5, 1, 20, 26]
+
+def dither(a_list):
+    """Take a list one step towards the ditherer's mean.
+    
+    Parameters
+    ----------
+    a_list : list
+        A list of numbers.
+    
+    Returns
+    -------
+    g_mean : float
+        Geometric mean of the list
+    a_mean : float
+        Arithmetic mean of the list
+    dithered : list
+        A copy of a_list, with the largest number replaced with the arithmetic
+        mean, and the smallest number replaced with the geometric mean."""
+    a_mean = np.mean(a_list)
+    g_mean = gmean(a_list)
+    dithered = [g_mean] + sorted(a_list)[1:-1] + [a_mean]
+    return (g_mean, a_mean, sorted(dithered))
+
+dither(trial)
+
+def d_mean(a_list, places):
+    """Take a list one step towards the ditherer's mean.
+    
+    Parameters
+    ----------
+    a_list : list
+        A list of numbers.
+    places : int
+        Desired precision for how many decimal places the arithmetic and 
+        geometric means should match to.
+    
+    Returns
+    -------
+    g_mean : float
+        Geometric mean of the list
+    a_mean : float
+        Arithmetic mean of the list
+    to_dither : list
+        The final list, with a sufficiently close a-mean and g-mean"""
+    to_dither = a_list
+    a_mean = np.mean(to_dither)
+    g_mean = gmean(to_dither)
+    while round(a_mean, places) != round(g_mean, places):
+        g_mean, a_mean, to_dither = dither(to_dither)
+    return (g_mean, a_mean, sorted(to_dither))
+
+d_mean(trial, 3)
